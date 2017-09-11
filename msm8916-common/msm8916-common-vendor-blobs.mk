7a7aa436395abddd3d5678188c9633d5e44392ca
Alberto97 <albertop2197@gmail.com>
2017-09-11 12:08:00 +0200

    [BACKPORT] msm8916-common: Remove debugging tools
    Commit taken from cm-14.1 branch, binary files left in repo,
    but adapted PRODUCT_COPY_FILES statement
    
    Change-Id: I9eaac5c76c30ae8ca323428c63c980955d4a29c1

--- msm8916-common/msm8916-common-vendor-blobs.mk
+++ msm8916-common/msm8916-common-vendor-blobs.mk
@@ -14,10 +14,7 @@
 
 PRODUCT_COPY_FILES += \
     vendor/motorola/msm8916-common/proprietary/bin/adsprpcd:system/bin/adsprpcd \
-    vendor/motorola/msm8916-common/proprietary/bin/batt_health:system/bin/batt_health \
     vendor/motorola/msm8916-common/proprietary/bin/btnvtool:system/bin/btnvtool \
-    vendor/motorola/msm8916-common/proprietary/bin/dbvc_atvc_property_set:system/bin/dbvc_atvc_property_set \
-    vendor/motorola/msm8916-common/proprietary/bin/hardware_revisions.sh:system/bin/hardware_revisions.sh \
     vendor/motorola/msm8916-common/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
     vendor/motorola/msm8916-common/proprietary/bin/irsc_util:system/bin/irsc_util \
     vendor/motorola/msm8916-common/proprietary/bin/netmgrd:system/bin/netmgrd \
